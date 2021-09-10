534a81bd54f69dc47be69a9bb5b3083b524cd445
Armin Doerzbach <ado@doerzbach.com>
2021-09-10 23:04:41 +0200

    the real reason for connection errors was the same label for mysql pod as for phpadmin pod

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,7 @@
 # Create local directory in VM which contains the Mysql DB
 sudo mkdir /mysqldata
 sudo chown 999:999 /mysqldata
-sudo cp $(dirname $0)/netfilter /usr/local/bin
-sudo cp $(dirname $0)/netfilter.service /etc/systemd/system
-sudo chmod 755 /usr/local/bin/netfilter
-sudo systemctl daemon-reload
-sudo systemctl enable netfilter
 
 export KUBECONFIG=$HOME/.kube/config
 kubectl apply -f ~/M153/mysql.yaml
 kubectl apply -f ~/M153/phpmyadmin.yaml
-
-sleep 60
-sudo reboot
